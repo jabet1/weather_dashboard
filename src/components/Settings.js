261eac3df1b8c3da618438d9184717c92ae7e4ee
Jean-Baptiste ABET <jean-baptiste.abet@mines-ales.org>
2021-04-28 20:05:39 +0200

    better ux

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,37 +1,59 @@
 import React, { Component } from 'react'
 import { connect } from "react-redux";
 import { addCity, removeCity } from "../actions/actions";
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import Table from 'react-bootstrap/Table';
+import {Button , Form, Table} from 'react-bootstrap/';
 
 class Settings extends Component {
-  
+
   state = {
     formValue : '',
+    formError: false,
+    cityNotInDBError: false,
+    cityDoubleError: false,
   }
      
   removeCity(city){
-    console.log('delete : ' + city)
-    this.props.dispatch(removeCity(city))
+    this.props.dispatch(removeCity(city));
   }
   addCity(city){
-    this.props.dispatch(addCity(city))
+    this.props.dispatch(addCity(city));
   }
 
   handleSubmit = (event) =>{
     event.preventDefault();
-    let value = this.state.formValue;
-    value = value.trim();
-    
-    this.props.dispatch(addCity(value))
-    this.setState({ city : '' });
+    let value = this.state.formValue.trim().toLowerCase();
+    value = value.charAt(0).toUpperCase() + value.slice(1); //first letter to uppercase
+
+    if(/^[a-zA-Z]*$/.test(value) && !this.props.cities.includes(value) && value.length > 0 ){
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${value}&APPID=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric`) //check if city is in Open Weather Map database
+      .then(res => {
+        if( !res.ok ){
+          this.setState({cityNotInDBError: true});
+        } else {
+          this.props.dispatch(addCity(value));
+          this.setState({ formValue : '', formError: false, cityNotInDBError: false, cityDoubleError:false });  
+        }
+      })
+      .then(
+        (error) => { 
+          console.log(error);
+        }
+      )
+    }else if(this.props.cities.includes(value)){
+      this.setState({cityDoubleError: true});
+      
+    }else{
+      this.setState({formError: true});
+    }
   }
 
   handleChange = (e) => {
-    this.setState({ formValue: e.target.value });
+    if(/^[a-zA-Z]*$/.test(e.target.value && e.target.value.length > 0)){
+      this.setState({ formValue: e.target.value, formError: false, cityNotInDBError: false, cityDoubleError: false });
+    }else{
+      this.setState({ formValue: e.target.value, formError: true });
+    }
   }
-
 
   render() {
     return (
@@ -43,9 +65,13 @@ class Settings extends Component {
             onChange = {this.handleChange}
             value = {this.state.formValue}
             placeholder = "Add a city"
-          />                 
-          <Button type='submit' className='col-2' >Valider</Button>
+            isInvalid = {this.state.formError}
+          />               
+          <Button type='submit' className='col-2' >Add</Button>
         </Form>
+        {this.state.formError &&  <Form.Text id="formErrorText" style={{color:'red'}} > A city name is only composed of letters (no number, no space, ...) </Form.Text>}
+        {this.state.cityNotInDBError &&  <Form.Text id="owmCityErrorText" style={{color:'red'}} > City was not found in OpenWeatherMap website </Form.Text>}
+        {this.state.cityDoubleError &&  <Form.Text id="cityDoubleErrorText" style={{color:'red'}} > Widget of {this.state.formValue} already present </Form.Text>}
         <Table  bordered hover className="mt-4" >
           <thead>
             <tr>
@@ -55,12 +81,12 @@ class Settings extends Component {
             </tr>
           </thead>
           <tbody>
-            {this.props.cities.map((v,index) => 
-              <tr key={v}>
+            {this.props.cities.map((cityName,index) => 
+              <tr key={cityName}>
                 <td>{index}</td>
-                <td>{v}</td>
+                <td>{cityName}</td>
                 <td>
-                  <Button variant="outline-danger" onClick={() => this.removeCity(v)}>
+                  <Button id="delete-button" variant="outline-danger" onClick={() => this.removeCity(cityName)}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
                       <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
                       <path fillRule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
@@ -71,7 +97,7 @@ class Settings extends Component {
             )}
           </tbody>
         </Table>
-
+ 
       </div>
     );
   }
--- src/components/Widget.js
+++ src/components/Widget.js
@@ -6,62 +6,63 @@ import '../font/weather-icons.min.css'
 
 class Widget extends Component {
     constructor(props){
-        super(props)
-        this.cityName = this.props.cityName
+        super(props);
+        this.cityName = this.props.cityName;
+        this.cityCountry = '';
         this.state = {
             temperature : 0,
             icon: '900',
             time: '12:00'
-        }
+        };
     }
 
-   /*componentDidMount() {
+   componentDidMount() {
         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.cityName}&APPID=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric`)
           .then(res => res.json())
           .then(
             (result) => {
-                const {weather : [{id : icon}], main : {temp : temp}, timezone : timezone} = result; // extract icon, temp and timezone from result
+                const {weather : [{id : icon}], main : {temp : temp}, timezone : timezone, sys : {country : country}} = result; // extract icon, temp, timezone and city country from result
                 const date = new Date();
                 let hour = (date.getUTCHours() + timezone/3600) % 24;
-                hour = hour >= 0 ? hour : hour + 24; 
+                let minutes = date.getUTCMinutes();
+                hour = hour >= 0 ? hour : hour + 24;
+                hour = hour < 10 ? '0' + hour : hour;
+                minutes = minutes < 10 ? '0'+ minutes : minutes;
+                this.cityCountry = country;
                 this.setState({
                     temperature: temp,
                     icon: icon,
-                    time: `${hour}:${date.getUTCMinutes()}`
+                    time: `${hour}:${minutes}`
                 });
-
             },
-            (error) => { // qu'est ce que j'en fais de ca ? 
-              this.setState({
-                error
-              });
+            (error) => { 
+              console.log(error)
             }
           )
-      }*/
+      }
 
     render(){
         return(
-          <div className="col-sm-6 p-1">
+          <div className="col-md-6 pt-2 ">
             <Card >
               <div className="row">
-                <div className="col-md-8 col-6"> 
-                  <div className="col-sm-12">
-                    {this.cityName}
+                <div className="col-md-8 col-6 my-auto"> 
+                  <div className="col-sm-12 ">
+                    {`${this.cityName},${this.cityCountry}`}
                   </div>
                   <div className="row">
-                    <div className="col-sm-6">
+                    <div className="col-md-6">
                       <i className="wi wi-thermometer"></i>
-                      {` ${this.state.temperature}°c `}
-                      
+                      {` ${this.state.temperature}°c `}                      
                     </div>
-                    <div className="col-sm-6">
+                    <div className="col-md-6">
                       <i className="wi wi-time-1"></i>
                       {` ${this.state.time} `}
                     </div>
                   </div> 
                 </div>   
                 <div className="col-md-4 col-6">
-                  <i className={`wi wi-owm-${this.state.icon} w-100 h-100 display-4`}></i>      
+                  <i className={`wi wi-owm-${this.state.icon} display-4 m-1` }></i>      
                 </div> 
               </div>
             </Card>
